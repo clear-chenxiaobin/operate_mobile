a6c8d002cfd5508121c4c19d274aa4491afa89e1
chenxiaobin <chenxiaobin@cleartv.cn>
2017-03-21 13:50:11 +0800

    new

---
+++ src/modules/app.js
@@ -0,0 +1,59 @@
+'use strict';
+
+(function () {
+    var app = angular.module('openvod', [
+        'ui.router',
+        'pascalprecht.translate',
+        'app.controllers',
+        'app.filters',
+        'app.directives',
+        'app.services',
+        'angular-md5',
+        'ngCookies',
+        'ngTable',
+        'ui.bootstrap',
+        'ui.bootstrap.datetimepicker',
+        'ng-fusioncharts'
+    ])
+    
+    .config(['$translateProvider', function ($translateProvider) {
+        var lang = navigator.language.indexOf('zh') > -1 ? 'zh-CN' : 'en-US';
+        $translateProvider.preferredLanguage(lang);
+        $translateProvider.useStaticFilesLoader({
+            prefix: 'i18n/',
+            suffix: '.json'
+        });
+    }])
+
+    .config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('/login');
+        $stateProvider
+            .state('login', {
+                url: '/login',
+                templateUrl: 'pages/login.html'
+            })
+            .state('app', {
+                url: '/app',
+                templateUrl: 'pages/app.html'
+            })
+            .state('app.online', {
+                url: '/online',
+                templateUrl: 'pages/online.html'
+            })
+            .state('app.home', {
+                url: '/home',
+                templateUrl: 'pages/home.html'
+            })
+    }])
+
+
+    .constant('CONFIG', {
+        // serverUrl: 'http://openvod.cleartv.cn/backend_clearmgt/v1/',
+        serverUrl: 'http://192.168.30.100/backend_clearmgt/v1/',
+        uploadImgUrl: 'http://mres.cleartv.cn/upload',
+        uploadVideoUrl: 'http://movies.clearidc.com/upload',
+        testUrl: 'test/',
+        test: false
+    })
+
+})();
---
+++ src/modules/controllers.js
@@ -0,0 +1,105 @@
+'use strict';
+
+(function() {
+    var app = angular.module('app.controllers', [])
+
+    .controller('indexController', ['$scope',
+        function($scope) {
+            var self = this;
+            self.init = function() {
+                this.maskUrl = '';
+            }
+        }
+    ])
+
+    .controller('loginController', ['$scope', '$http', '$state', '$filter', 'md5', 'util',
+        function($scope, $http, $state, $filter, md5, util) {
+            var self = this;
+            self.init = function() {
+
+            }
+            
+            self.login = function () {
+                self.loading = true;
+                
+                var data = JSON.stringify({
+                    username: self.userName,
+                    password: md5.createHash(self.password)
+                })
+                $http({
+                    method: 'POST',
+                    url: util.getApiUrl('logon', '', 'server'),
+                    data: data
+                }).then(function successCallback(response) {
+                    var msg = response.data;
+                    if (msg.rescode == '200') {
+                        util.setParams('token', msg.token);
+                        self.getEditLangs();
+                    } 
+                    else {
+                        alert(msg.rescode + ' ' + msg.errInfo);
+                    }
+                }, function errorCallback(response) {
+                    alert('连接服务器出错');
+                }).finally(function(value) {
+                    self.loading = false;
+                });
+            }
+            // 
+            self.getEditLangs = function() {
+                $http({
+                    method: 'GET',
+                    url: util.getApiUrl('', 'editLangs.json', 'local')
+                }).then(function successCallback(response) {
+                    util.setParams('editLangs', response.data.editLangs);
+                    $state.go('app.home');
+                }, function errorCallback(response) {
+
+                });
+            }
+
+        }
+    ])
+
+
+    .controller('appController', ['$http', '$scope', '$state', '$stateParams', 'util', 'CONFIG',
+        function($http, $scope, $state, $stateParams, util, CONFIG) {
+            var self = this;
+            self.init = function() {
+
+                // 弹窗层
+                self.maskUrl = '';
+                self.maskParams = {};
+            }
+
+            // 添加 删除 弹窗，增加一个样式的class
+            self.showHideMask = function(bool,url){
+                // bool 为true时，弹窗出现
+                if (bool) {
+                    $scope.app.maskUrl = url;
+                    $scope.app.showMaskClass = true;
+                } else {
+                    $scope.app.maskUrl = '';
+                    $scope.app.showMaskClass = false;
+                }
+            }
+
+        }
+    ])
+
+    //HOME
+    .controller('homeController', ['$http', '$scope', '$state', '$location','$filter', '$stateParams', '$q', 'util', 'CONFIG',
+        function($http, $scope, $state, $location, $filter, $stateParams, $q, util, CONFIG) {
+            var self = this;
+            self.init = function() {
+
+            }
+
+            self.logout = function(event) {
+                // util.setParams('token', '');
+                $state.go('login');
+            }
+        }
+
+    ])
+})();
